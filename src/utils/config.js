3459a835ffb64b6df9eefba9f2df69c509448045
RyanBailes <bailesmarketingco@gmail.com>
2018-04-22 22:49:47 -0500

    added

---
+++ src/utils/config.js
@@ -0,0 +1,23 @@
+const colors = require("../styles/colors");
+
+module.exports = {
+  pathPrefix: "/",
+  appName: "SimpleLanding",
+  siteTitle: "SimpleLanding - a landing page GatsbyJs starter",
+  siteUrl: "https://gssl.greglobinski.com",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  siteDescription: "SimpleLanding is a dead simple landing page GatsbyJs starter.",
+  contactEmail: "hello@example.com",
+  ctaLinkUrl: "https://github.com/greglobinski/gatsby-starter-simple-landing",
+  ctaLinkLabel: "Go to GitHub",
+  // manifest.json
+  manifestName: "SimpleLanding - Gatsby starter",
+  manifestShortName: "SimplLanding",
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.bg,
+  manifestThemeColor: colors.bg,
+  manifestDisplay: "standalone",
+  // analytics
+  analyticsTrackingId: "UA-110088221-3"
+};
